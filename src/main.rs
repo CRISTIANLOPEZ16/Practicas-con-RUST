515e476b221ac53794fe0a3a20041fa8e587f933
NewLifeOrNewDeath <crdavidlg16@gmail.com>
2022-07-05 11:20:13 -0500

    Calculadora con expresiones regulares

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,77 @@
-use std::io;
+use regex::Regex;
 fn main() {
-    println!("Digita la temperatura maxima");
-    // En esta parte defino con claridad el tipo de dato
-    let mut tmax:String= String::new();
-    io::stdin().read_line(&mut tmax).unwrap();
-    println!("Digita la temperatura minima");
-    // En esta parte no defino con claridad el tipo de dato
-    let  mut tmin=String::new();
-    io::stdin().read_line(&mut tmin).unwrap();
-    let tmaxi:u8=tmax.trim().parse().unwrap();
-    let tmini:i8=tmin.trim().parse().unwrap();
+    println!("Hola amigo");
+    //creo las expresiones  regex
+    //Antes de toda expresion viene la letra r  
+    let re_add=Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_sub=Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
+    let re_mul=Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    //solicito datos
+    println!("Por favor introduzca su expresion");
+    let mut expression=String::new();
+    std::io::stdin().read_line(&mut expression).unwrap();
 
-    if tmini < -5 {
-        println!("Que pinche frio amigo mio, temperatura minima {} ",tmini);
-    }else{
-        let promedio:i8=(tmaxi as i8+tmini)/2;
-        println!("ta bien el promedio del clima {}",promedio);
+
+    
+    //aplicar operacion de multiplicación
+    loop{
+        let caps = re_mul.captures(expression.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+        let cap_expression=caps.get(0).unwrap().as_str();
+        let left_value:i32=caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value:i32=caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let addition = left_value * right_value;
+
+        expression=expression.replace(cap_expression,&addition.to_string());
     }
+
+
+
+
+
+    //aplicar operacion de resta
+    loop{
+        let caps = re_sub.captures(expression.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+        let cap_expression=caps.get(0).unwrap().as_str();
+        let left_value:i32=caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value:i32=caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let addition = left_value - right_value;
+
+        expression=expression.replace(cap_expression,&addition.to_string());
+    }
+
+
+
+
+    
+    //aplicar operacion de suma
+    loop{
+        let caps = re_add.captures(expression.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+        let cap_expression=caps.get(0).unwrap().as_str();
+        let left_value:i32=caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value:i32=caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let addition = left_value + right_value;
+
+        expression=expression.replace(cap_expression,&addition.to_string());
+    }
+    
+    println!("El resultado de tu operacion es : {}",expression);
     
 }
